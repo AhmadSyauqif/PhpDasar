7739bf537906f84b998aebba3f79a0fee742bacd
Ahmad_syauqi <54980526+AhmadSyauqif@users.noreply.github.com>
2021-10-07 08:12:44 +0700

    Add files via upload

---
+++ PhpDasar/halaman/home.php
@@ -0,0 +1,4 @@
+<div class="halaman">
+	<h2>Halaman Depan</h2>
+    <p>Selamat datang di website kami</p>
+</div>
---
+++ PhpDasar/halaman/tentang.php
@@ -0,0 +1,5 @@
+<div class="Halaman">
+<h2>Tentang Kami</h2>
+<p>Ini adalah halaman tentang kami</p>
+	<p>kami telah membuat beribu website yang sangat berkulitas </p>
+</div>
